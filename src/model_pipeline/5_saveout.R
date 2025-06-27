d55d95641dd881e8dc6c935b602715f6334c1238
Henry Kyle <henry@nexusquanta.co.nz>
2025-06-27 16:19:11 +1200

    fix(pipeline): improve logic around console printing of saved outputs

--- src/model_pipeline/5_saveout.R
+++ src/model_pipeline/5_saveout.R
@@ -11,58 +11,59 @@ if(!isFALSE(param_saveout_tables) && length(param_saveout_tables) > 0) {
     param_saveout_tables
   )
   
+  print("Emissions estimation completed.")
+  
   if(length(tables_to_save) > 0) {
     
     # create output dir
     if (!dir.exists(param_output_path)) {
       dir.create(param_output_path, recursive = TRUE)
     }
-  }
   
-  if(param_output_data_format == "csv") {
-    
-    print("Emissions estimation completed.")
-    
-    # Save out all tables as CSV files
-    for (table_name in param_saveout_tables) {
-      table_df <- get(paste0(table_name, "_df"))
-      write_csv(
-        table_df,
+    if(param_output_data_format == "csv") {
+      
+      # Save out all tables as CSV files
+      for (table_name in param_saveout_tables) {
+        table_df <- get(paste0(table_name, "_df"))
+        write_csv(
+          table_df,
+          file.path(
+            param_output_path,
+            paste0(table_name, "_", sys_datetime, ".csv")
+          )
+        )
+        
+        print(paste0("Results saved out to ", file.path(param_output_path, paste0(table_name, "_", sys_datetime, ".csv"))))
+      }
+      
+    } else if(param_output_data_format == "json") {
+      
+      # saveout one json file
+      write_json(
+        tables_to_save,
         file.path(
           param_output_path,
-          paste0(table_name, "_", sys_datetime, ".csv")
-        )
+          paste0("output_", sys_datetime, ".json")
+        ),
+        digits=NA
       )
       
-      print(paste0("Results saved out to ", file.path(param_output_path, paste0(table_name, "_", sys_datetime, ".csv"))))
+      print(paste0("Results saved out to ",
+                   file.path(param_output_path, paste0("output_", sys_datetime, ".json"))))
       
     }
     
-  } else if(param_output_data_format == "json") {
+    # print to console any cases of saved out tables having zero rows:
     
-    # saveout one json file
-    write_json(
-      tables_to_save,
-      file.path(
-        param_output_path,
-        paste0("output_", sys_datetime, ".json")
-      ),
-      digits=NA
-    )
-    
-    print(paste0("Emissions estimation completed. Results saved out to ",
-                 file.path(param_output_path, paste0("output_", sys_datetime, ".json"))))
-    
-  }
-  
-  # print to console any cases of saved out tables having zero rows:
-  
-  for (table_name in param_saveout_tables) {
-    table_df <- get(paste0(table_name, "_df"))
-    if (nrow(table_df) == 0) {
-      print(paste0("Note: Table '", table_name, "' has zero rows as no farm data inputs for this module were provided"))
+    for (table_name in param_saveout_tables) {
+      table_df <- get(paste0(table_name, "_df"))
+      if (nrow(table_df) == 0) {
+        print(paste0("Note: Table '", table_name, "' has zero rows as no farm data inputs for this module were provided"))
+      }
+      
     }
-  }
+    
+  } 
   
 } else {
   
